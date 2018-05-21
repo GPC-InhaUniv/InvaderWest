5f47e94824fd25507ca058650725826ae6eb7baa
qwo330 <qwd330@naver.com>
2018-05-21 11:43:17 +0900

    StageTest 1.1

---
+++ Timevader/Assets/02.Scripts/DragRotation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragRotation : MonoBehaviour {
+    public GameObject Earth;
+
+    Vector2 PrevPoint;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0) || Input.touchCount == 1)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Mouse Click");
+                PrevPoint = Input.mousePosition;
+                //new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Debug.Log("touch Click");
+                PrevPoint = Input.GetTouch(0).position;
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                Debug.Log("Mouse Drag");
+                //TouchStatus = "Touch Began";
+
+                Earth.transform.Rotate(Input.mousePosition.y - PrevPoint.y, -(Input.mousePosition.x - PrevPoint.x), 0);
+                PrevPoint = Input.mousePosition;
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                Debug.Log("Touch Drag");
+                //TouchStatus = "Touch Began";
+
+                Earth.transform.Rotate(Input.GetTouch(0).position.y - PrevPoint.y, -(Input.GetTouch(0).position.x - PrevPoint.x), 0);
+                PrevPoint = Input.GetTouch(0).position;
+            }
+            
+        }
+        else
+        {
+            Earth.transform.Translate(0, 0, 0);
+            // TouchStatus = "Idle";
+        }
+	}
+}
---
+++ Timevader/Assets/02.Scripts/DragTest.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DragTest : MonoBehaviour {
+
+    //public Canvas canvas;
+
+    public GraphicRaycaster gr;
+    PointerEventData ped;
+
+    Vector3 prevPoint;
+    public Image[] StageIcon;
+
+    [Range(-1,1)]
+    public int dirValue;
+
+    private void Start()
+    {
+        //gr = canvas.GetComponent<GraphicRaycaster>();
+        ped = new PointerEventData(null);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ped.position = Input.mousePosition;
+        List<RaycastResult> results = new List<RaycastResult>();
+        gr.Raycast(ped, results);
+        if (results.Count != 0 && results[0].gameObject.tag == "TouchAble")
+        {
+            Debug.Log(Input.mousePosition);
+            Debug.Log("Raycast Hit");
+            ClickDrag();
+            //TouchSlide();
+            ChangeStageInfo();
+        }
+    }
+
+    void ChangeStageInfo()
+    {
+        float rotateValue = transform.rotation.z;
+    }
+
+    void ClickDrag()
+    {
+        /*    PC 조작   */
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("click");
+            prevPoint = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Drag");
+
+            // float dragValue = (prevPoint.x - Input.mousePosition.x) / 5 - (prevPoint.y - Input.mousePosition.y);
+            float dragValue = (transform.localScale.x);
+            // r^2 = x^2 + y^2
+            Vector3 rotatePower = new Vector3(0, 0, dirValue * dragValue * 2 / 3);
+
+            //Debug.Log(dragValue);
+            transform.Rotate(rotatePower);
+            prevPoint = Input.mousePosition;
+        }
+    }
+
+    void TouchSlide()
+    {
+        /*    폰 조작   */
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Debug.Log("Touch");
+            prevPoint = Input.mousePosition;
+        }
+        if (Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Debug.Log("Slide");
+            float dragValue = (prevPoint.x - Input.GetTouch(0).position.x) - (prevPoint.y - Input.GetTouch(0).position.y);
+            Vector3 rotatePower = new Vector3(0, 0, dragValue / 2);
+
+            transform.Rotate(rotatePower * Time.deltaTime);
+            prevPoint = Input.GetTouch(0).position;
+        }
+    }
+}
