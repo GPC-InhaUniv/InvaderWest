f540e6743c8c49c5abac03dc70ad37f81a4265e7
qwo330 <qwd330@naver.com>
2018-05-22 15:55:38 +0900

    Enemy 설계 중 1.0

---
+++ Timevader/Assets/02.Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    Line,
+    Circle, // 특정 좌표를 기준으로 회전
+    Zigzag, // 지그재그
+    //diagonal Line // 대각선
+}
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject Invader;
+
+    EnemyModel enemyBase;
+
+	public Enemy()
+    {
+        enemyBase = new EnemyModel();
+    }
+
+    public Enemy InvaderFactory(Direction dir, Vector3 destination)
+    {
+        GameObject newObj = Instantiate(Invader);
+    }
+    
+}
