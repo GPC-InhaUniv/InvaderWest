9454d3502dd8558f644704410f1b3b595a53dfd7
황윤우 <open1765@gmail.com>
2018-05-30 15:32:49 +0900

    PIWT-9  상점UI

---
+++ Timevader/Assets/02.Scripts/Data/GamsSceneManger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamsSceneManger : MonoBehaviour {
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void GoLogInScene()
+    {
+        SceneManager.LoadScene("LogIn");
+    }
+    public void GoMainScene()
+    {
+        SceneManager.LoadScene("Main");
+
+    }
+    public void GoShipSelectScene()
+    {
+        SceneManager.LoadScene("ShipSelect");
+
+    }
+    public void GoShopScene()
+    {
+
+    }
+    public void GoStageSelectScene()
+    {
+
+    }
+    public void GoStoryScene()
+    {
+
+    }
+
+}
