ad9edc24d52dcebda2d1afa4190abfa116d23437
DESKTOP-VE3SNON\hrd32122 <ygt4545@gmail.com>
2018-05-30 19:56:49 +0900

    5/30 인트로 ui작업 김판주

---
+++ Timevader/Assets/02.Scripts/DrageEarthRotation.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DrageEarthRotation : MonoBehaviour {
+
+    public GameObject Earth;
+
+    Vector3 prevPoint;
+
+    public GraphicRaycaster GraphicRayScreen;
+    PointerEventData pointerEventData;
+
+    [Range(-0.5f,5f)]
+    public float RotateSpeed = 1f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit) && hit.collider.tag == "TouchAble")
+            {
+                // 지구 회전
+                Debug.Log("Raycast Hit");
+                ClickDrag();
+                //TouchSlide();
+            }
+        }
+    }
+
+
+    void ClickDrag()
+    {
+        /*    PC 조작   */
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("click");
+            prevPoint = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Drag");
+            float dragValue = (prevPoint.x - Input.mousePosition.x) / 5 - (prevPoint.y - Input.mousePosition.y);
+            // r^2 = x^2 + y^2
+            Vector3 rotatePower = new Vector3(0, 0, dragValue);
+
+            //Debug.Log("roat" + dragValue);
+            Earth.transform.Rotate(rotatePower / 2 * RotateSpeed);
+            prevPoint = Input.mousePosition;
+        }
+    }
+
+    void TouchSlide()
+    {
+        /*    폰 조작   */
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Debug.Log("Touch");
+            prevPoint = Input.mousePosition;
+        }
+        if (Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Debug.Log("Slide");
+            float dragValue = (prevPoint.x - Input.GetTouch(0).position.x) / 5 - (prevPoint.y - Input.GetTouch(0).position.y);
+            Vector3 rotatePower = new Vector3(0, 0, dragValue);
+
+            Earth.transform.Rotate(rotatePower / 2 * RotateSpeed * Time.deltaTime);
+            prevPoint = Input.GetTouch(0).position;
+        }
+    }
+}
