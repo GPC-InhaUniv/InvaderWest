19fda43ea4e44848d03d675cf0258d4727a4b99a
박설 <psps3020@gmail.com>
2018-06-07 18:05:44 +0900

    6/7 박설 스토리, 난이도 선택 UI 하는중

---
+++ Timevader/Assets/02.Scripts/Story.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Story : MonoBehaviour {
+    [SerializeField]
+    private Text easyText;
+    [SerializeField]
+    private Button easyBtn;
+    [SerializeField]
+    private GameObject SelectPanel;
+    [SerializeField]
+    private GameObject ConfirmPanel;
+
+    public void NextPage()
+    {
+        
+    }
+
+    public void SelectEasy()
+    {
+        easyText.text = "500000";
+        easyBtn.interactable=false;
+        
+    }
+
+    public void SelectNormal()
+    {
+        
+    }
+
+    public void SelectHard()
+    {
+        
+    }
+
+    public void SelectOK()
+    {
+        //AccountInfo.ChangeRestTimeData(50000);
+        //SceneManager.LoadScene("Main");
+    }
+
+    public void SelectCancel()
+    {
+        ConfirmPanel.SetActive(false);
+    }
+}
