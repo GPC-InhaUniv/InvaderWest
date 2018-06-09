c5c8ff90339e763bbdadebe69f8ec13c8df7d49c
박설 <psps3020@gmail.com>
2018-06-09 17:55:13 +0900

    6/9 박설 아이템시작

--- Timevader/Assets/02.Scripts/Player/Player.cs
+++ Timevader/Assets/02.Scripts/Player/Player.cs
@@ -18,10 +18,38 @@ public class Player : MonoBehaviour {
     public GameObject Shot;
     public Transform ShotSpawn;
 
+    public GameObject AddedShot;
+    public Transform AddedSpawn;
+
     public float fireDelta = 0.5f;
 
     private float nextFire = 0.5f;
     private float myTime = 0.0f;
+
+    Rigidbody rigidbody;
+    Vector3 movement;
+
+    public void GetItem(DropItem.ItemKind itemKind)
+    {
+        switch (itemKind)
+        {
+            case DropItem.ItemKind.AddMissileitem:
+                AddMissile();
+                break;
+            case DropItem.ItemKind.Assistantitem:
+                break;
+        }
+    }
+
+    void AddMissile()
+    {
+        Instantiate(Shot, ShotSpawn.position, ShotSpawn.rotation);
+    }
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
@@ -39,20 +67,27 @@ public class Player : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        //if (Input.anyKeyDown)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+            movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+            rigidbody.velocity = movement * speed;
 
-        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
-        rigidbody.velocity = movement * speed;
-
-        rigidbody.position = new Vector3
-        (
-            Mathf.Clamp(rigidbody.position.x, Boundary.xMin, Boundary.xMax),
-            Mathf.Clamp(rigidbody.position.y, Boundary.yMin, Boundary.yMax),
-            0.0f
-        );
-        
+            rigidbody.position = new Vector3
+            (
+                Mathf.Clamp(rigidbody.position.x, Boundary.xMin, Boundary.xMax),
+                Mathf.Clamp(rigidbody.position.y, Boundary.yMin, Boundary.yMax),
+                0.0f
+            );
+        }
+        /*
+        else
+        {
+            movement = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(movement.x, movement.y, 0.0f);
+        }
+        */
     }
 }
