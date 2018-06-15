f558e0ecac618f900008e548b48fe6b065399ef6
박설 <psps3020@gmail.com>
2018-06-15 15:48:58 +0900

    PIWT-46 6/15 박설 스토리 구현중

---
+++ Timevader/Assets/02.Scripts/Seol/DraggingShip.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggingShip : MonoBehaviour {
+
+    float middleValue = 100.0f;
+    float increaseValue = 1.0f;
+
+    //지구를 돌리는
+    [SerializeField]
+    GameObject ship;
+    
+    Vector3 prevPoint;
+
+    float ConditionRotation;
+
+    [SerializeField]
+    [Range(-0.5f, 5f)]
+    float rotateSpeed;
+
+    [SerializeField]
+    Camera MainCamera;
+    
+    [SerializeField] //로그인 조건 만족시 사라짐
+    GameObject[] otherObject;
+
+    void FixedUpdate()
+    {
+        ActionClickOrDarg();
+    }
+    private void Start()
+    {
+        ConditionRotation = 0.90f;
+    }
+
+    void ActionClickOrDarg()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Debug.Log("클릭");
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // 지구 회전
+                Debug.Log("Raycast Hit");
+                ClickOrDrag();
+                //TouchSlide();
+            }
+        }
+    }
+
+    void ClickOrDrag()
+    {
+        /*    PC 조작   */
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("click");
+            prevPoint = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Drag");
+            float dragValue = (prevPoint.x - Input.mousePosition.x) / 5 - (prevPoint.y - Input.mousePosition.y);
+            // r^2 = x^2 + y^2
+            Vector3 rotatePower = new Vector3(0, 0, dragValue);
+
+            //Debug.Log("roat" + dragValue);
+            ship.transform.Rotate(rotatePower / 2 * rotateSpeed);
+            prevPoint = Input.mousePosition;
+        }
+    }
+
+    void TouchSlide()
+    {
+        /*    폰 조작   */
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Debug.Log("Touch");
+            prevPoint = Input.mousePosition;
+        }
+        if (Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Debug.Log("Slide");
+            float dragValue = (prevPoint.x - Input.GetTouch(0).position.x) / 5 - (prevPoint.y - Input.GetTouch(0).position.y);
+            Vector3 rotatePower = new Vector3(0, 0, dragValue);
+
+            ship.transform.Rotate(rotatePower / 2 * rotateSpeed * Time.deltaTime);
+
+            prevPoint = Input.GetTouch(0).position;
+        }
+    }    
+
+    void HieObject()
+    {
+        for (int i = 0; i < otherObject.Length - 1; i++)
+        {
+            otherObject[i].SetActive(false);
+        }
+        ship.SetActive(false);
+    }    
+    
+}
