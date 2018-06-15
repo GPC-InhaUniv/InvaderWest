369fed10e5758b83376f6e6a4ebfaa589c07378c
DESKTOP-VE3SNON\hrd32122 <ygt4545@gmail.com>
2018-06-15 15:18:19 +0900

    6월15일 - 김판주 - 폴더 경로 수정 중

--- Timevader/Assets/02.Scripts/PANJOO/ShipChoiceController.cs
+++ Timevader/Assets/02.Scripts/PANJOO/ShipChoiceController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class ShipChoiceNum : MonoBehaviour {
+public class ShipChoiceController : MonoBehaviour {
 
     [SerializeField]
     GameObject[] LifeImages;
