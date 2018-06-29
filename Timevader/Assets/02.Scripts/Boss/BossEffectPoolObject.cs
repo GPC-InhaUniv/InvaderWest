5aecd95c4fa9b4e6fe8b93b0f0aa9a5cd91e6e16
황윤우 <open1765@gmail.com>
2018-06-29 17:31:28 +0900

    no message

--- Timevader/Assets/02.Scripts/Boss/BossEffectPoolObject.cs
+++ Timevader/Assets/02.Scripts/Boss/BossEffectPoolObject.cs
@@ -50,12 +50,9 @@ public class BossEffectPoolObject : MonoBehaviour {
     public void ReturnToPool(GameObject obj)
     {
         obj.SetActive(false);
-<<<<<<< HEAD
-=======
 
 
         PoolObjsEffect.Enqueue(obj);
-        
->>>>>>> d9bf4262da837228b84a2e47c9b2bc1d4e629750
+
     }
 }
